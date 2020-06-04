be086f335a34726986b0dad86c17e023f7e04879
johnMedlockDev <john.medlock.dev@gmail.com>
2020-06-04 15:34:01 -0500

    added tests to sparksession added to git ignore created files for other tests

---
+++ src/test/java/com/github/p3spark/operation1/CountyOilTest.java
@@ -0,0 +1,22 @@
+package com.github.p3spark.operation1;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CountyOilTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void findTotOilByCounty() {
+    }
+}
---
+++ src/test/java/com/github/p3spark/operation1/SimpleTransformTest.java
@@ -0,0 +1,26 @@
+package com.github.p3spark.operation1;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimpleTransformTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void filterCompanyName() {
+    }
+
+    @Test
+    void allCompanyName() {
+    }
+}
