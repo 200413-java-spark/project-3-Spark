fb9e1467363ef7f6c2b8a1a6d591c08c48927f34
Sparce Zissef <phuctqle@gmail.com>
2020-06-10 11:30:03 -0500

    commenting out some code, reworking ideas

--- src/main/java/com/github/p3spark/operation1/CountyOil.java
+++ src/main/java/com/github/p3spark/operation1/CountyOil.java
@@ -3,7 +3,9 @@ package com.github.p3spark.operation1;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.spark.network.protocol.Encoders;
 import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Encoder;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.types.DataTypes;
@@ -16,46 +18,47 @@ public class CountyOil {
 
         //select columns for counties and oil produced and year
         String[] headers = dataCSV.columns();//gets headers into a string array
-        int cate = 17;
-        int depv = 15;
-        int indv = 16;
+        int category = 1;
+        int depVariable = 15;
+        int indVariable = 16;
         //selects those three columns
-        Dataset<Row> data1 = dataCSV.select(headers[cate], headers[depv], headers[indv]).na().drop();
+        Dataset<Row> data1 = dataCSV.select(headers[category], headers[depVariable], headers[indVariable]).na().drop();
         Dataset<Row> data = data1
-            .withColumn(headers[depv], data1.col(headers[depv]).cast(DataTypes.DoubleType))//replaces strings with doubles
-            .withColumn(headers[indv], data1.col(headers[indv]).cast(DataTypes.DoubleType))
+            .withColumn(headers[depVariable], data1.col(headers[depVariable]).cast(DataTypes.DoubleType))//replaces strings with doubles
+            .withColumn(headers[indVariable], data1.col(headers[indVariable]).cast(DataTypes.DoubleType))
             .cache();
         data.createOrReplaceTempView("oilByYear");
         data.printSchema();
         //counts how many counties there are
-        int CCount = (int)data.select(headers[cate]).distinct().count();
+        int CCount = (int)data.select(headers[category]).distinct().count();
             List<Row> counties = new ArrayList<>();
-            counties = data.select(headers[cate]).distinct().takeAsList(CCount);
+            counties = data.select(headers[category]).distinct().takeAsList(CCount);
 
         List<Dataset<Row>> addList = new ArrayList<>();
 
-        //for (int i = 0; i < CCount; i++){
+        //Dataset<Row> creatingNew = data.map(func, Encoders.Strings.class);
+        /*for (int i = 0; i < CCount; i++){
             //iterates through each county and shows a table of how much oil
             //was produced each year
-            Dataset<Row> pickTable = session.sql("SELECT "+headers[depv]+
-            ", "+headers[indv]+" FROM oilByYear WHERE "+headers[cate]+
+            Dataset<Row> pickTable = session.sql("SELECT "+headers[depVariable]+
+            ", "+headers[indVariable]+" FROM oilByYear WHERE "+headers[category]+
             "=\'"+counties.get(0).get(0).toString().replace("'", "\'\'")+"\'").cache();
             pickTable.createOrReplaceTempView("pickTable");
             //sums up the totals
-            Dataset<Row> oilYear = session.sql("SELECT "+headers[indv]+
-            " ,ROUND(SUM("+headers[depv]+
-            "),2) AS Total_Oil FROM pickTable GROUP BY "+headers[indv]);
+            Dataset<Row> oilYear = session.sql("SELECT "+headers[indVariable]+
+            " ,ROUND(SUM("+headers[depVariable]+
+            "),2) AS Total_Oil FROM pickTable GROUP BY "+headers[indVariable]);
             oilYear.createOrReplaceTempView("ysTable");
-            Dataset<Row> oilYearSorted = session.sql("SELECT * FROM ysTable ORDER BY "+headers[indv]);
+            Dataset<Row> oilYearSorted = session.sql("SELECT * FROM ysTable ORDER BY "+headers[indVariable]);
             //shows the table
             //oilYearSorted.show();
             //System.out.println(counties.get(i).get(0).toString().replace("'", "\'\'"));
             //TimeUnit.SECONDS.sleep(5);//pauses for 5 seconds
             addList.add(oilYearSorted);
-        //}
+        }*/
         
             //shows the table
-            addList.get(Integer.parseInt(arg2)).show();
-            System.out.println(counties.get(Integer.parseInt(arg2)).get(0).toString().replace("'", "\'\'"));
+            //addList.get(Integer.parseInt(arg2)).show();
+            //System.out.println(counties.get(Integer.parseInt(arg2)).get(0).toString().replace("'", "\'\'"));
     }
 }
