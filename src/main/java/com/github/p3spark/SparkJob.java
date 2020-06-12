4f172dcbd42340f3fe9f232f7062d915b7701de9
johnMedlockDev <john.medlock.dev@gmail.com>
2020-06-12 15:07:53 -0500

    removed tests refactored main method, fixed config fix

--- src/main/java/com/github/p3spark/SparkJob.java
+++ src/main/java/com/github/p3spark/SparkJob.java
@@ -1,26 +1,9 @@
 package com.github.p3spark;
 
-import com.github.p3spark.io.Database;
-import com.github.p3spark.operation1.SimpleTransform;
-import com.github.p3spark.startup.Consumer;
-import com.github.p3spark.startup.CreateSparkSession;
-import com.github.p3spark.startup.DataReader;
-
-import org.apache.spark.sql.SparkSession;
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
+import com.github.p3spark.utils.Init;
 
 public class SparkJob {
     public static void main(String[] args) {
-        //Creates spark session
-        CreateSparkSession startSession = CreateSparkSession.getInstance(); // Starts SparkSession
-        SparkSession session = startSession.getSession(); // pulls a reference to the session
-
-        new Consumer().builder(session);
-        session.close();
-
+        new Init();
     }
-
 }
